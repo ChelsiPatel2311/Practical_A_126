319a3a45010b7bf78dfc04262fac066f6c56af6f
ChelsiPatel2311 <chelsipatel2311@gmail.com>
2026-02-22 17:52:53 +0530

    Added inverted right half pyramid pattern

--- practical_3.py
+++ practical_3.py
@@ -8,3 +8,18 @@ for i in range(1, rows+1):
         if num > 9:
             num = 1
     print()
+# set-a
+# Inverted Right Half Pyramid Pattern
+
+rows = 5
+num = 1
+
+for i in range(rows):
+    for j in range(rows - i):
+        print(num, end=" ")
+        num += 1
+        
+        # Reset number after 9
+        if num == 10:
+            num = 1
+    print()
