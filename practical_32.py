7d09d75ef85b30f62375329e41c4551eae10d550
ChelsiPatel2311 <chelsipatel2311@gmail.com>
2026-02-22 21:39:32 +0530

    Updated practical file - Chelsi

--- practical_32.py
+++ practical_32.py
@@ -1,8 +1,12 @@
-# Full Pyramid
-
 rows = 5
 
 for i in range(1, rows + 1):
+    
+    # Print spaces (for alignment)
+    print(" " * (rows - i), end="")
+    
+    # Print stars
     for j in range(i):
-        print("*", end=" ")
+        print("* ", end="")
+    
     print()
