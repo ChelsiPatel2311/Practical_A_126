608ed15c8b11951703ee3b8b088c48ba70690ef6
ChelsiPatel2311 <chelsipatel2311@gmail.com>
2026-02-23 22:35:34 +0530

    add success

--- practical_32.py
+++ practical_32.py
@@ -10,3 +10,8 @@ for i in range(1, rows + 1):
         print("* ", end="")
     
     print()
+# Practical 4B
+for i in range(5, 0, -1):        
+    for j in range(i):           
+        print(chr(65 + j), end=" ")
+    print()                      
