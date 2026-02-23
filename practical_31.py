34f8602b4b45bccaeb12aa22e26b11ec9d8d8b96
ChelsiPatel2311 <chelsipatel2311@gmail.com>
2026-02-23 22:33:44 +0530

    add success

--- practical_31.py
+++ practical_31.py
@@ -6,3 +6,16 @@ for i in range(rows, 0, -1):
     for j in range(i):
         print("*", end=" ")
     print()
+# Practical 4B code 
+num = 1
+
+for i in range(1, 6):      
+    for j in range(i):     
+        print(num, end=" ")
+        
+        num += 1
+        
+        if num == 10:      
+            num = 1
+            
+    print()                
