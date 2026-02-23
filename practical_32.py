bfbaa6a9bedb85dff64df159976784b1c15d478f
ChelsiPatel2311 <chelsipatel2311@gmail.com>
2026-02-23 22:40:43 +0530

    add success

--- practical_32.py
+++ practical_32.py
@@ -11,7 +11,14 @@ for i in range(1, rows + 1):
     
     print()
 # Practical 4B
-for i in range(5, 0, -1):        
-    for j in range(i):           
+rows = 5
+
+for i in range(rows):
+    # Print leading spaces
+    print(" " * i, end="")
+    
+    # Print letters
+    for j in range(rows - i):
         print(chr(65 + j), end=" ")
+    
     print()                      
